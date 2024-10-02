d1a9b7cf27c560a8884d2f985617e19c78d87f9d
mugund10 <bjmugundhan@gmail.com>
2024-10-02 18:49:52 +0530

    ch2_s

---
+++ ch2/1_basic.rs
@@ -0,0 +1,22 @@
+fn main() {
+    //the curly bracket is the placeholder for 10
+    //we cant simply print the integers in rust
+    println!("{}",10);
+
+    //print multiple lines without \n and println!
+    print!("hi guys
+how are u
+i
+am
+fine
+");
+
+    //learning backslash
+    println!("hi guys\"");
+    //it's like omitting its function of whatever that comes after that
+    println!("\\n \\n\
+hi bro");
+
+
+}
+
